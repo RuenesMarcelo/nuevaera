be45dc70836cd0aa6f6cd81c4954668182307d6a
Marcelo Ruenes <marcel-013@hotmail.com>
2025-10-26 23:09:43 -0500

    Se deja funcional el menu hamburguesa para pantallas pequeñas

--- src/app/Domains/shared/components/header/header/header.component.ts
+++ src/app/Domains/shared/components/header/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, signal } from '@angular/core';
+import { Component, HostListener, inject, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
@@ -16,34 +16,65 @@ import { Product } from '../../../models/product';
 })
 export class HeaderComponent {
 
-  
 
-  
-  categories= signal<Category[]>([]);
+  categories = signal<Category[]>([]);
   products = signal<Product[]>([]);
   private CategoriesService = inject(CategoriesService);
   private ProductService = inject(ProductService);
+  isMobileNavOpen = signal(false);
+  showScrollTop = signal(false);
 
-  ngOnInit(): void {    
-    this.getCategories();
-
-    
-  }
 
   constructor(public authService: AuthService) {
   }
 
-  private getCategories(){
-    this.CategoriesService.getAll()
-    .subscribe({
-      next: (data) =>{
-        this.categories.set(data);
-      },
-      error: () =>{
-        
-      }
-    })
+
+
+  ngOnInit(): void {
+    this.getCategories();
+
+
   }
+
+  toggleMobileNav() {
+    this.isMobileNavOpen.update(value => !value);
+    document.body.classList.toggle('mobile-nav-active', this.isMobileNavOpen());
+  }
+
+  closeMobileNavOnLink(event: Event) {
+    const target = event.target as HTMLElement;
+    if (target.tagName === 'A' && this.isMobileNavOpen()) {
+      this.toggleMobileNav();
+    }
+  }
+
+  toggleDropdown(event: Event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+
+    const toggle = event.currentTarget as HTMLElement;
+    const parent = toggle.parentElement; // <li>
+    const dropdown = parent?.nextElementSibling as HTMLElement;
+
+    parent?.classList.toggle('active');
+    dropdown?.classList.toggle('dropdown-active');
+  }
+
+
+  private getCategories() {
+    this.CategoriesService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.categories.set(data);
+        },
+        error: () => {
+
+        }
+      })
+  }
+
+
+
 
 
 }
