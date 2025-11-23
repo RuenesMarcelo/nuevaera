f58ff79ee0190929ba1617e341cc024e37ea25af
Marcelo Ruenes <marcel-013@hotmail.com>
2025-11-23 10:11:52 -0500

    Se agrega archivo de sitemap, se modifica el título y se deja funcional el agregar productos

--- src/app/Domains/shared/components/pages/card-service/card-service.component.ts
+++ src/app/Domains/shared/components/pages/card-service/card-service.component.ts
@@ -15,13 +15,13 @@ export class CardServiceComponent {
   services_info: Service_info[] = [
   {
     titulo: 'Material de laboratorio',
-    descripcion: 'Ofrecemos una alta variedad de lmaterial de laboratorio para practicas escolares y universitárias con elementos de fabricación y extranjera de la mejor calidad',
+    descripcion: 'Ofrecemos una alta variedad de material de laboratorio para prácticas escolares y universitárias con productos de fabricación nacional y extranjera de la mejor calidad, manejamos grandes cantidades.',
     foto: 'assets/icons/Material_laboratorio.png'
 
   },
   {
     titulo: 'Reparación de material de laboratorio',
-    descripcion: 'Reparamos y damos matenimiento a materiales de laboratorio contando con calidad y mano de obra capacitadaen la manipulación y reparación de elementos fabricados en vidrio',
+    descripcion: 'Reparamos y damos matenimiento a materiales de laboratorio contando con calidad y mano de obra capacitada en la manipulación y reparación de elementos fabricados en vidrio.',
     foto: 'assets/icons/material_roto.png'
 
   },
@@ -31,27 +31,27 @@ export class CardServiceComponent {
     foto: "assets/icons/Reactivos.png"
   }, {
     titulo: 'Biologia',
-    descripcion: 'Productos para la manipulación de material biológico en prácticas y laboratorios que emplean ingeniería',
+    descripcion: 'Productos para la manipulación de material biológico en prácticas y laboratorios que se emplean en los campos de química y de Biologia. No pases por alto las precauciones y las buenas prácticas en tus laboratorios de anatomia y biología.',
     foto: 'assets/icons/Biologia.png'
   },
   {
     titulo: 'Cosméticos y limpieza',
-    descripcion: 'Ofrecemos una amplia gama de productos cosméticos para el  contar con amplio catálogo de productos de limpieza corporal y limpieza doméstica',
+    descripcion: 'Ofrecemos una amplia gama de productos cosméticos, te invitamos aver nuestro amplio catálogo de productos de limpieza corporal y limpieza del hogar.',
     foto: 'assets/icons/Cosmeticos_limpieza.png'
   },
   {
     titulo: 'Productos agrícolas',
-    descripcion: 'Pregunta por inmunizantes, perticidas, fertilizantes i demás insumos relacionados con agronomia, realizamos envios a cualquier parte del pais y damos acompañamiento',
+    descripcion: 'Pregunta por inmunizantes, perticidas, fertilizantes i demás insumos relacionados con agronomia, realizamos envios a cualquier parte del pais y damos acompañamiento.',
     foto: 'assets/icons/Prod_agricola.png'
   },
   {
     titulo: 'Bioseguridad y protección',
-    descripcion: 'Consigue los mejores equipos de protección y bioseguridad para hacer tus laboratorios de la manera mas segura, manejamos la mejor calidad y a petición y requerimiento de nuestros clientes',
+    descripcion: 'Consigue los mejores equipos de protección y bioseguridad para hacer tus laboratorios de la manera mas segura, manejamos la mejor calidad y a petición y requerimiento de nuestros clientes.',
     foto: 'assets/icons/Seguridad_y_proteccion.png'
   },
   {
     titulo: 'Asesorias',
-    descripcion: 'Si necesitas acompañamiento y orientación en la elaboración de algun producto quimico o de practica de laboratorio puedes escribirnos y comentarnos cual es tu necesidad para poder echarte una mano!',
+    descripcion: 'Si necesitas acompañamiento y orientación en la elaboración de algun producto quimico o de practica de laboratorio puedes escribirnos y comentarnos cual es tu necesidad para poder echarte una mano!.',
     foto: 'assets/icons/Asesorias.png'
   }
 
